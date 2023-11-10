b5b644879a69fd43e970dc6065c50b1392d2b9d2
Svyatoslav Shypka <svyat200909@gmail.com>
2023-11-10 12:37:15 +0100

    Finished

--- PolitechLista2/Run.cpp
+++ PolitechLista2/Run.cpp
@@ -8,57 +8,28 @@ int main()
 {
     CNumber c_num_0, c_num_1, c_num_2;
 
-    srand((unsigned)time(NULL));
-    for (int i = 0; i < 1000; i++) {
-        c_num_1 = rand() % (100000) - 1000 / 2;
-        //c_num_1 = 38649;
-        //c_num_2 = 35;
-        c_num_2 = rand() % (100000) - 1000 / 2;
+        c_num_1 = 32;
+        c_num_2 = 2;
 
         // -
         c_num_0 = c_num_1 - c_num_2;
         cout << c_num_1.sToStr() << " - " << c_num_2.sToStr() << " = ";
-        cout << (c_num_0.sToStr());
-        if (c_num_0.vGetValue() == c_num_1.vGetValue() - c_num_2.vGetValue()) {
-            cout << "  -Ok" << endl;
-        }
-        else {
-            cout << "  *********************************************************INCORRECT" << endl;
-        }
+        cout << (c_num_0.sToStr()) << endl;
 
         // +
         c_num_0 = c_num_1 + c_num_2;
         cout << c_num_1.sToStr() << " + " << c_num_2.sToStr() << " = ";
-        cout << (c_num_0.sToStr());
-        if (c_num_0.vGetValue() == c_num_1.vGetValue() + c_num_2.vGetValue()) {
-            cout << "  -Ok" << endl;
-        }
-        else {
-            cout << "  *********************************************************INCORRECT" << endl;
-        }
+        cout << (c_num_0.sToStr()) << endl;
 
         // *
         c_num_0 = c_num_1 * c_num_2;
         cout << c_num_1.sToStr() << " * " << c_num_2.sToStr() << " = ";
-        cout << (c_num_0.sToStr());
-        if (c_num_0.vGetValue() == c_num_1.vGetValue() * c_num_2.vGetValue()) {
-            cout << "  -Ok" << endl;
-        }
-        else {
-            cout << "  *********************************************************INCORRECT" << endl;
-        }
+        cout << (c_num_0.sToStr()) << endl;
 
         // /
         c_num_0 = c_num_1 / c_num_2;
         cout << c_num_1.sToStr() << " / " << c_num_2.sToStr() << " = ";
-        cout << (c_num_0.sToStr());
-        if (c_num_0.vGetValue() == c_num_1.vGetValue() / c_num_2.vGetValue()) {
-            cout << "  -Ok" << endl;
-        }
-        else {
-            cout << "  *********************************************************INCORRECT" << endl;
-        }
-    }
+        cout << (c_num_0.sToStr()) << endl;
 
     return 0;
 }
